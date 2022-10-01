e1f37b56f096cc5080ffaf84eff9af1c95a78f85
unknown <famouszegenee@gmail.com>
2022-10-01 10:47:12 +0100

    refactored the textfield

--- lib/portrait.dart
+++ lib/portrait.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:loss_of_revenue_calculator/reusable_textfield.dart';
+
 const textStyle = TextStyle(
   fontSize: 16.0,
   color: Color(0xFFA5672B),
@@ -403,55 +405,6 @@ class _PortraitViewState extends State<PortraitView> {
             ],
           ),
 
-    );
-  }
-}
-
-// ignore: must_be_immutable
-class ReusableTextField extends StatelessWidget {
-  ReusableTextField(
-      {Key? key,
-      required this.kontroller,
-      required this.hintTex,
-      required this.labText})
-      : super(key: key);
-
-  final TextEditingController kontroller;
-  String labText;
-  String hintTex;
-
-  @override
-  Widget build(BuildContext context) {
-    return TextField(
-      keyboardType: TextInputType.number,
-      controller: kontroller,
-      style: const TextStyle(
-        fontSize: 14.0,
-      ),
-      minLines: 1,
-      maxLines: 5,
-      obscureText: false,
-      decoration: InputDecoration(
-          contentPadding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-          border: const OutlineInputBorder(
-            borderRadius: BorderRadius.all(Radius.circular(32.0)),
-          ),
-          focusedBorder: const OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Color(0xFFA5672B,),
-            ),
-          ),
-          enabledBorder: const OutlineInputBorder(
-            borderSide: BorderSide(
-              color: Color(0xFFA5672B), width: 1.0
-            ),
-            borderRadius: BorderRadius.all(Radius.circular(32.0)),
-          ),
-          labelStyle: const TextStyle(
-            color: Color(0xFFA5672B),
-          ),
-          labelText: labText,
-          hintText: hintTex),
     );
   }
 }
